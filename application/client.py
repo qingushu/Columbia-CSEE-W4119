58d940d6c2eb8c26315393a9998c378607c667e7
Gushu Qin <gushuqin@Gushus-MacBook-Pro.local>
2025-04-29 16:12:55 -0400

    remove ballot options from tracker, integrated it with server and client, fix host port stiff

--- application/client.py
+++ application/client.py
@@ -1,32 +1,36 @@
+import sys
+import time
+from network_layer.peer import Peer
 from client_ui import ClientUi
-import sys 
-
-# import peer from ...
 
 class Client:
-    def __init__(self, client_network_port, client_addr, 
-                       server_addr, server_port):
+    def __init__(self, client_network_port, client_addr, server_addr, server_port):
         self.peer_port = client_network_port
         self.peer_addr = client_addr
-        self.blockchain = None
-        self.ballot_options = ['A', 'B', 'C'] # Dummy for testing. TODO: Change to None
+        self.server_addr = server_addr
+        self.server_port = server_port
+
+        self.peer = Peer(tracker_port=self.server_port, local_port=self.peer_port)
+        self.peer.connect()
+        self.peer.listen()
+        
+        time.sleep(1)  # Give peer time to request and receive ballot
+
+        self.ballot_options = self.peer.voting_options
         self.ui = ClientUi(self)
 
 if __name__ == '__main__':
-    client_network_port = int(sys.argv[1]) # port nubmer for p2p communication
-    client_addr = sys.argv[2]
+    if len(sys.argv) != 5:
+        print("Usage: python client.py <client_network_port> <client_addr> <server_port> <server_addr>")
+        sys.exit(1)
+
+    client_network_port = int(sys.argv[1])
+    client_addr = sys.argv[2]  # currently unused but reserved for future
     server_port = int(sys.argv[3])
-    server_addr = sys.argv[4]
+    server_addr = sys.argv[4]  # currently unused as tracker_host is fixed in peer.py
 
-    # initialize client and ui
     client = Client(client_network_port, client_addr, server_addr, server_port)
-    
-    # TODO: Initialize connection
-    # client.peer.connect() -> Should register with the tracker
-    # self.ballot_options = client.peer.request_ballot() -> Should request for candidates
-
-    # TODO: For testing purposes, creating a dummy peer. Remove later
     client.ui.run_ui()
-    
+
 
 
